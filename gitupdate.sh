d9f7a58930aa2923564b538ee48f525514478509
bill <wtahler@gmail.com>
2021-08-04 19:26:39 -0700

    auto commit

---
+++ gitupdate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /root/config
+
+git add .
+git status
+
+#echo -n "Enter the Description for the Change: " [Minor Update]
+read CHANGE_MSG
+
+#git commit -m "${CHANGE_MSG}"
+git commit -m "auto commit"
+git push -u origin master
+exit
